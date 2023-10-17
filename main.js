218dcb251bb1f32b249ec91a96d809603c2afb15
Rodrigo <rodrigo@addmind.me>
2023-10-17 20:12:05 -0300

    Update Gyroscope Handler

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Testando javascript');
+if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', handleOrientation, false);
+    console.log('Gyroscope data will be logged.');
+  
+    function handleOrientation(event) {
+      const gamma = event.gamma;  // Rotation around the z-axis (left to right)
+      const beta = event.beta;    // Rotation around the x-axis (front back)
+  
+      console.log('Gamma (Z-axis rotation):', gamma);
+      console.log('Beta (X-axis rotation):', beta);
+    }
+  } else {
+    console.log('DeviceOrientationEvent is not supported on this device.');
+  }
