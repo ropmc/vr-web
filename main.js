123f54ae9fc66d431f95ac7dc54b42ca490fcf80
Rodrigo <rodrigo@addmind.me>
2023-10-18 02:15:00 -0300

    Mensagem

--- main.js
+++ main.js
@@ -3,33 +3,31 @@ AFRAME.registerShader('barrel-distortion', {
       src: { type: 'map', is: 'uniform' },
     },
     vertexShader: `
-      attribute vec4 position;
-      attribute vec2 uv;
-      uniform mat4 modelViewMatrix;
-      uniform mat4 projectionMatrix;
-      varying vec2 vUv;
+      varying vec2 vUv;  // Define vUv as varying
   
       void main() {
-        gl_Position = projectionMatrix * modelViewMatrix * position;
+        // Update the vertex position and uv
         vUv = uv;
+        vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );  // Update the position
+  
+        gl_Position = projectionMatrix * mvPosition;  // Update gl_Position
       }
     `,
     fragmentShader: `
-      precision highp float;
-      uniform sampler2D src;
+      uniform sampler2D inputTexture;
       varying vec2 vUv;
-  
+    
       void main() {
         float scale = 0.9; // Adjust scale as needed
         float distortion = 0.9; // Adjust distortion factor as needed
         vec2 uv = vUv * 2.0 - 1.0;
         vec2 uv2 = uv * (1.0 + scale * dot(uv, uv));
         uv2 = (uv2 + 1.0) / 2.0;
-  
+    
         // Apply barrel distortion
         uv2 = uv2 * (1.0 + distortion * pow(length(uv2), 2.0));
-  
-        gl_FragColor = texture2D(src, uv2);
+    
+        gl_FragColor = texture2D(inputTexture, uv2);
       }
     `
   });
