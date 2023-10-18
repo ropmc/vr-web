2c7f97bc0d8db26bcf79259049e9f0ce5ec1b99f
Rodrigo <rodrigo@addmind.me>
2023-10-18 02:10:02 -0300

    Mensagem

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the <html> element
-    const htmlElement = document.querySelector('html');
-  
-    // Check if the <html> element has the 'a-fullscreen' class
-    if (htmlElement.classList.contains('a-fullscreen')) {
-      // Remove the 'a-fullscreen' class
-      htmlElement.classList.remove('a-fullscreen');
-    }
-  });
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the element with the specified class
-    const element = document.querySelector('a-canvas.a-grab-cursor');
-  
-    // Check if the element is found
-    if (element) {
-      // Set the CSS properties
-      element.style.position = 'relative';
-      element.style.width = '100vw';
-      element.style.height = '100vh';
-    } else {
-      console.error('Element with class ".a-canvas.a-grab-cursor" not found.');
-    }
+AFRAME.registerShader('barrel-distortion', {
+    schema: {
+      src: { type: 'map', is: 'uniform' },
+    },
+    vertexShader: `
+      // Your vertex shader code here
+    `,
+    fragmentShader: `
+      // Your fragment shader code here
+    `
   });
