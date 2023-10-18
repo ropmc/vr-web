0a4c8ab74b75cef6a2f9a62ca9a04e07e8feb439
Rodrigo <rodrigo@addmind.me>
2023-10-18 13:16:55 -0300

    1316

--- play-on-click.js
+++ play-on-click.js
@@ -4,10 +4,10 @@ AFRAME.registerComponent('play-on-click', {
       this.onClick = this.onClick.bind(this);
     },
     play: function () {
-      window.addEventListener('click', this.onClick);
+      document.body.addEventListener('click', this.onClick);
     },
     pause: function () {
-      window.removeEventListener('click', this.onClick);
+      document.body.removeEventListener('click', this.onClick);
     },
     onClick: function (evt) {
       var videoEl = this.el.getAttribute('material').src;
